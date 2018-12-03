8e352a23c4b3d3f01c032f03eade4f94497ce3d9
HMPerson1 <hmperson1@gmail.com>
2018-12-03 09:44:50 -0500

    Add Django ORM models

--- chess_server/chess/models.py
+++ chess_server/chess/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=200, unique=True)
+
+class Game(models.Model):
+    DEFAULT_BOARD_STATE = ''
+
+    GS_WHITE_TURN = 'TW'
+    GS_BLACK_TURN = 'TB'
+    GS_WHITE_WON = 'EW'
+    GS_BLACK_WON = 'EB'
+    GS_DRAW = 'ED'
+    GAME_STATE_CHOICES = (
+        ('in-progress', (
+            (GS_WHITE_TURN, "White's turn"),
+            (GS_BLACK_TURN, "Black's turn"),
+        )),
+        ('ended', (
+            (GS_WHITE_WON, "White won"),
+            (GS_BLACK_WON, "Black won"),
+            (GS_DRAW, "Draw"),
+        )),
+    )
+
+    black_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='game_black_set')
+    black_present = models.BooleanField(default=True)
+    white_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='game_white_set')
+    white_present = models.BooleanField(default=True)
+    board_state = models.CharField(max_length=64, default=DEFAULT_BOARD_STATE)
+    game_state = models.CharField(
+        max_length=2,
+        choices=GAME_STATE_CHOICES,
+        default=GS_WHITE_TURN,
+    )

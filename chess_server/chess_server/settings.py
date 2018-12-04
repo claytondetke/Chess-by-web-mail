52c5f0a50e5f19980395fada1f1fd15fd8750423
HMPerson1 <hmperson1@gmail.com>
2018-12-04 01:25:25 -0500

    Don't use WSGI in debug

--- chess_server/chess_server/settings.py
+++ chess_server/chess_server/settings.py
@@ -43,8 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mod_wsgi.server',
 ]
+
+if not DEBUG:
+    INSTALLED_APPS.append('mod_wsgi.server')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
